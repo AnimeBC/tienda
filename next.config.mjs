b3ac821efd7b32ad1eca46dab3ee964e0d1f840f
brayan <brayan@gmail.com>
2024-07-03 13:58:05 -0500

    10%

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { dev, isServer }) => {
+      if (dev && !isServer) {
+        config.watchOptions = {
+          poll: 1000, // Verificar cambios cada 1 segundo
+          aggregateTimeout: 300, // Retrasar la recarga en 300ms
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
