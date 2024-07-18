d8182c9c89e8748765918c27c0148cb2d8e12533
brayan <brayan@gmail.com>
2024-07-18 13:55:00 -0500

    60&

--- _componentes/_Barra/Barra.js
+++ _componentes/_Barra/Barra.js
@@ -1,109 +1,125 @@
 "use client";
 import estilos from "./barra.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
-export default function Barra({ datos,FidiomaSeleccionado,idiomaSeleccionado,Fseleccion,seleccion}) {
+
+export default function Barra({
+  datos,
+  FidiomaSeleccionado,
+  idiomaSeleccionado,
+  Fseleccion,
+  seleccion,
+}) {
   const [mostrar, Fmostrar] = useState(0);
   const [iconoActivo, FiconoActivo] = useState(null);
+  const [flechaPos, FflechaPos] = useState("50%");
+  const [fixed, setFixed] = useState(false);
+
   const datosIdioma = Object.values(datos["idiomas"]).find(
     (item) => Object.keys(item)[0] === idiomaSeleccionado
   )[idiomaSeleccionado];
+
   function cambiarIcono(a, valor) {
     Fmostrar(valor);
     FiconoActivo(valor);
+    const rect = a.currentTarget.getBoundingClientRect();
+    const parentRect = a.currentTarget.parentElement.getBoundingClientRect();
+    const offset = rect.left + rect.width / 2 - parentRect.left;
+    FflechaPos(`${offset}px`);
   }
+
   function resetIcono() {
     Fmostrar(0);
     FiconoActivo(null);
+    FflechaPos("50%");
   }
-  //const nombresIdiomas = datos["idiomas"].map((obj) => Object.keys(obj)[0]);
+
   const nombresIdiomas = datos["idiomas"].map((obj) => Object.keys(obj)[0]);
-  const subcategorias = Object.keys(datosIdioma)
-  console.log(Object.values(datos["idiomas"]));
+  const subcategorias = Object.keys(datosIdioma);
+  const datosDeSubCategorias = Object.values(datosIdioma);
+
+  useEffect(() => {
+    if (mostrar !== 0) {
+      const subFlotante = document.querySelector(`.${estilos.subFlotante}`);
+      if (subFlotante) {
+        subFlotante.style.setProperty('--flecha-pos', flechaPos);
+      }
+    }
+  }, [flechaPos, mostrar]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setFixed(true);
+      } else {
+        setFixed(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={estilos.todo}>
+    <div className={`${estilos.todo} ${fixed ? estilos.fixed : ''}`}>
       <div className={estilos.logo}>BITEL</div>
       <div className={estilos.navegacion}>
         <nav className={estilos.navegacionA}>
           <ul className={estilos.navegacionul}>
-            <li
-              onMouseEnter={(a) => cambiarIcono(a, 1)}
-              onMouseLeave={resetIcono}
-            >
-              {subcategorias[0]}
-              <ion-icon
-                name={
-                  iconoActivo === 1 ? "caret-up-outline" : "caret-down-outline"
-                }
-              ></ion-icon>
-              {mostrar === 1 && (
-                <div className={estilos.flotante}>
-                  <div>ejemplo 1</div>
-                  <div>ejemplo 1</div>
-                </div>
-              )}
-            </li>
-            <li
-              onMouseEnter={(a) => cambiarIcono(a, 2)}
-              onMouseLeave={resetIcono}
-            >
-               {subcategorias[1]}
-              <ion-icon
-                name={
-                  iconoActivo === 2 ? "caret-up-outline" : "caret-down-outline"
-                }
-              ></ion-icon>
-              {mostrar === 2 && (
-                <div className={estilos.flotante}>
-                  <div>ejemplo 2</div>
-                  <div>ejemplo 2</div>
-                </div>
-              )}
-            </li>
-            <li
-              onMouseEnter={(a) => cambiarIcono(a, 3)}
-              onMouseLeave={resetIcono}
-            >
-             {subcategorias[2]}
-              <ion-icon
-                name={
-                  iconoActivo === 3 ? "caret-up-outline" : "caret-down-outline"
-                }
-              ></ion-icon>
-              {mostrar === 3 && (
-                <div className={estilos.flotante}>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                  <div>ejemplo 3</div>
-                </div>
-              )}
-            </li>
-            <li
-              onMouseEnter={(a) => cambiarIcono(a, 4)}
-              onMouseLeave={resetIcono}
-            >
-                {idiomaSeleccionado}
-              <ion-icon
-                name={
-                  iconoActivo === 4 ? "caret-up-outline" : "caret-down-outline"
-                }
-              ></ion-icon>
-              {mostrar === 4 && (
-                <div className={estilos.flotante}>
-                  <div className={estilos.flotanteA}>
-                    <ul className={estilos.flotanteulZ}>
-                      {nombresIdiomas.map((a, index) => (
-                        <li key={index} className={estilos.flotanteulliZ}>
-                          <Link href={`#${a}`} onClick={()=>{FidiomaSeleccionado(a);Fmostrar(0);FiconoActivo(null)}}>{a}</Link>
-                        </li>
-                      ))}
-                    </ul>
+            {subcategorias.map((subcategoria, index) => (
+              <li
+                key={index}
+                onMouseEnter={(a) => cambiarIcono(a, index + 1)}
+                onMouseLeave={resetIcono}
+              >
+                {subcategoria}
+                <ion-icon
+                  name={
+                    iconoActivo === index + 1
+                      ? "caret-up-outline"
+                      : "caret-down-outline"
+                  }
+                ></ion-icon>
+                {mostrar === index + 1 && (
+                  <div className={estilos.subFlotante}>
+                    {Object.values(datosDeSubCategorias[index]).map(
+                      (a, b) => (
+                        <div key={b} className={estilos.otros}>{a}</div>
+                      )
+                    )}
                   </div>
+                )}
+              </li>
+            ))}
+            <li
+              onMouseEnter={(a) => cambiarIcono(a, subcategorias.length + 1)}
+              onMouseLeave={resetIcono}
+            >
+              {idiomaSeleccionado}
+              <ion-icon
+                name={
+                  iconoActivo === subcategorias.length + 1
+                    ? "caret-up-outline"
+                    : "caret-down-outline"
+                }
+              ></ion-icon>
+              {mostrar === subcategorias.length + 1 && (
+                <div className={estilos.subFlotante}>
+                  {nombresIdiomas.map((a, index) => (
+                    <div key={index} className={estilos.idiomas}>
+                      <Link
+                        href={`#${a}`}
+                        onClick={() => {
+                          FidiomaSeleccionado(a);
+                          Fmostrar(0);
+                          FiconoActivo(null);
+                        }}
+                      >
+                        {a}
+                      </Link>
+                    </div>
+                  ))}
                 </div>
               )}
             </li>
